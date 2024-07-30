d4c2510be70a470e3e97805326b7b87dba3b0242
Austin Ziegler <aziegler@kineticcommerce.com>
2024-07-29 21:02:45 -0400

    feat: KineticLib.CaseTransform
    This is probably *not* worth releasing unless we decide to make our own
    case transformation library, but it still provides value and can be
    useful for learning from.
    
    Apache 2.0

---
+++ test/kinetic_lib/case_transform_test.exs
@@ -0,0 +1,10 @@
+defmodule KinticLib.CaseTransformTest do
+  alias KineticLib.CaseTransform
+
+  use ExUnit.Case
+
+  doctest CaseTransform
+
+  # TODO(20231110az): add more case transformation examples
+  # Remember to avoid the weaknesses of Inflex
+end
